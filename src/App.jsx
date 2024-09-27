e38278067717aff3be1a0daa053f24e376dcb514
Stalloyde <stalloyde93@gmail.com>
2024-09-27 23:10:21 +0800

    Style initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
 
   const [cuisineA, setCuisineA] = useState(cuisines[0]);
   const [cuisineB, setCuisineB] = useState(
-    cuisines[Math.floor(Math.random() * (cuisines.length - 1)) + 1],
+    cuisines[Math.floor(Math.random() * (cuisines.length - 1)) + 1]
   );
   const [userLocation, setUserLocation] = useState(null);
   const [gameOver, setGameOver] = useState(false);
@@ -48,7 +48,7 @@ function App() {
         },
         (error) => {
           console.error('Error getting user location:', error);
-        },
+        }
       );
     } else {
       console.error('Geolocation is not supported by this browser.');
@@ -58,14 +58,14 @@ function App() {
 
   function removeTarget(e) {
     const target = cuisines.findIndex(
-      (elem) => elem === e.target.dataset.remove,
+      (elem) => elem === e.target.dataset.remove
     );
     cuisines.splice(target, 1);
   }
 
   function redirectGoogleMaps(cuisine) {
     window.open(
-      `https://www.google.com/maps/search/?api=1&query=${cuisine}&location=${userLocation.latitude},${userLocation.longitude}&radius=10000`,
+      `https://www.google.com/maps/search/?api=1&query=${cuisine}&location=${userLocation.latitude},${userLocation.longitude}&radius=10000`
     );
   }
 
@@ -74,7 +74,7 @@ function App() {
       shuffleArray(cuisines);
       setCuisineA(cuisines[0]);
       setCuisineB(
-        cuisines[Math.floor(Math.random() * (cuisines.length - 1) + 1)],
+        cuisines[Math.floor(Math.random() * (cuisines.length - 1) + 1)]
       );
     } else {
       setCuisineA(cuisines[0]);
@@ -91,38 +91,46 @@ function App() {
   return (
     <>
       {gameOver ? (
-        <div className='gameOver'>
+        <div>
           <div>
             <h1>Winning Cuisine: {cuisineA}</h1>
           </div>
           <p>Refresh page to restart</p>
         </div>
       ) : (
-        <>
-          <div className='description'>
-            <h1>What to eat?</h1>
-            <p>
+        <div className="flex">
+          <div className="max-w-[600px] border-r border-black">
+            <h1 className="text-2xl font-bold">What To Eat?</h1>
+            <p className="py-5">
               This app helps you figure out what to eat. Perfect for those
               needing a meal, but don't know what they want.
             </p>
-            <h2>How it works:</h2>
-            <p>
+            <h2 className="text-xl font-bold">How it works:</h2>
+            <p className="py-5">
               Simply pick the cuisine that you prefer over the other. The app
               harnesses the power of elimination, filtering out undesirable
               options until a winning cuisine is determined. A list of nearby
               eateries will be generated based on the winning option.
             </p>
           </div>
-          <div className='container'>
-            <button data-remove={cuisineB} onClick={(e) => handleClick(e)}>
+          <div className="flex items-center gap-5 p-10">
+            <button
+              data-remove={cuisineB}
+              onClick={(e) => handleClick(e)}
+              className="h-[100px] w-[200px] rounded-l-full border-orange-600 bg-orange-400 p-1 transition duration-300 ease-in-out hover:-translate-y-4 hover:scale-110 hover:border-2 hover:bg-orange-500"
+            >
               {cuisineA}
             </button>
             <p>or..</p>
-            <button data-remove={cuisineA} onClick={(e) => handleClick(e)}>
+            <button
+              data-remove={cuisineA}
+              onClick={(e) => handleClick(e)}
+              className="h-[100px] w-[200px] rounded-r-full border-orange-600 bg-orange-400 p-1 transition duration-300 ease-in-out hover:-translate-y-4 hover:scale-110 hover:border-2 hover:bg-orange-500"
+            >
               {cuisineB}
             </button>
           </div>
-        </>
+        </div>
       )}
     </>
   );
